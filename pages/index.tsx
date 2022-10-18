4be456aee6d6048656c0286628591c12d8f5145f
bsdaoquang <bsdaoquang@gmail.com>
2022-10-19 04:35:56 +0700

    re build

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,11 @@
-import { Card } from "antd";
+import { Card } from 'antd'
 import React from 'react'
 
 const HomePage = () => {
   return (
     <div className="container mt-5">
       <Card>
-        <h2>Deploy NextJS to Netlify</h2>
+        <h2>Netlify auto build when push to main branch github</h2>
       </Card>
     </div>
   )
